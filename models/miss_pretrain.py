e3f03c1c5451b1f89b6d2ddf7e133213a42068a5
JIA CHEN <125014501+TIMMY-CHAN@users.noreply.github.com>
2024-06-30 20:44:38 +0800

    Update miss_pretrain.py

--- models/miss_pretrain.py
+++ models/miss_pretrain.py
@@ -44,7 +44,7 @@ class MISS_Pretrain(nn.Module):
         self.tokenizer = init_tokenizer()   
         encoder_config = BertConfig.from_json_file(med_config)
         encoder_config.encoder_width = vision_width
-        self.text_encoder = BertModel.from_pretrained('/home/cjw/demo/bert-base-uncased',config=encoder_config, add_pooling_layer=False)
+        self.text_encoder = BertModel.from_pretrained('bert-base-uncased',config=encoder_config, add_pooling_layer=False)
         self.text_encoder.resize_token_embeddings(len(self.tokenizer)) 
 
         text_width = self.text_encoder.config.hidden_size
@@ -83,8 +83,8 @@ class MISS_Pretrain(nn.Module):
         # create the decoder
         decoder_config = BertConfig.from_json_file(med_config)
         decoder_config.encoder_width = vision_width        
-        self.text_decoder = BertLMHeadModel.from_pretrained('/home/cjw/demo/bert-base-uncased',config=decoder_config)
-        #self.text_decoder = BertModel.from_pretrained('/home/cjw/demo/bert-base-uncased',config=decoder_config)    
+        self.text_decoder = BertLMHeadModel.from_pretrained('bert-base-uncased',config=decoder_config)
+        #self.text_decoder = BertModel.from_pretrained('bert-base-uncased',config=decoder_config)    
         self.text_decoder.resize_token_embeddings(len(self.tokenizer)) 
         tie_encoder_decoder_weights(self.text_encoder,self.text_decoder.bert,'','/attention')
         
